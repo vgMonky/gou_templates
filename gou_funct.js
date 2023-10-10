99ca56d89598905050bff7bb48974c27996d77c5
vgMonky <126716555+vgMonky@users.noreply.github.com>
2023-10-09 23:42:12 -0300

    service interactive

--- gou_funct.js
+++ gou_funct.js
@@ -22,3 +22,17 @@ function sendEmail(){
       message => alert("Message sent succesfully")
     );
   }
+
+
+let card = document.getElementsByClassName("card")
+console.log(card)
+let description = document.getElementsByClassName("description")
+console.log(description)
+
+
+function textShow0() {description[0].style.display = "block" }
+function textNone0() {description[0].style.display = "none" }
+function textShow1() {description[1].style.display = "block" }
+function textNone1() {description[1].style.display = "none" }
+function textShow2() {description[2].style.display = "block" }
+function textNone2() {description[2].style.display = "none" }
