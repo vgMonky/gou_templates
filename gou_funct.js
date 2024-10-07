006d14eb8665df483b56aa14ba915f99966278ff
Vik <victorgou.design@gmail.com>
2024-10-07 13:46:25 -0300

    contact updated

--- gou_funct.js
+++ gou_funct.js
@@ -6,23 +6,6 @@ function onMenuClick() {
     navbar.classList.toggle(responsive_class_name);
 }
 
-function sendEmail(){
-    Email.send({
-      Host : "smtp.elasticemail.com",
-      Username : "victorgou.design@gmail.com",
-      Password : "72EFB8A185FA7B31D65A7AFA8789BE0B3B5D",
-      To : 'victorgou.design@gmail.com',
-      From : 'andregoudschaal@gmail.com',
-      Subject : "NEW GOU FORM",
-      Body : "Name: " + document.getElementById("fullname").value 
-          + "<br> Company: " + document.getElementById("company").value
-          + "<br> Email: " + document.getElementById("email").value
-          + "<br> Message: " + document.getElementById("message").value
-    }).then(
-      message => alert("Message sent succesfully")
-    );
-  }
-
 
 let card = document.getElementsByClassName("card")
 console.log(card)
